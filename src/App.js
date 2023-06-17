8a1f2800ef46d4a112a4ea61b14999f713e8f1ed
Farhan Ali Mirza <farhanalimirza2003@gmail.com>
2023-06-17 20:33:01 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
+
+function App() {
+  const [cartIsShown, setcartIsShown] = useState(false);
+
+  const showCardHandler = () => {
+    setcartIsShown(true);
+  };
+  const hideCardHandler = () => {
+    setcartIsShown(false);
+  };
+
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCardHandler} />}
+      <Header onShowCart={showCardHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
